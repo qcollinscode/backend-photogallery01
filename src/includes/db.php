ac576cfb8d01aab5b5aec61999841fe4efdb09a4
qcollinscode <qcollins@qraynix.com>
2017-04-25 13:31:46 -0400

    corrected bug

--- src/includes/db.php
+++ src/includes/db.php
@@ -1,9 +1,17 @@
 <?php
 
-$db['db_host'] = 'localhost';
-$db['db_user'] = 'root';
-$db['db_pass'] = '';
-$db['db_name'] = 'photogallery';
+
+$db['db_host'] = 'us-cdbr-iron-east-03.cleardb.net';
+$db['db_user'] = 'ba5909f71c5065';
+$db['db_pass'] = '964b71d0';
+$db['db_name'] = 'heroku_404e34a302806a2';
+
+
+
+// $db['db_host'] = 'localhost';
+// $db['db_user'] = 'root';
+// $db['db_pass'] = '';
+// $db['db_name'] = 'photogallery';
 
 foreach($db as $key => $value) {
     define(strtoupper($key), $value);
@@ -16,9 +24,3 @@ if(!$connection) {
 }
 
 ?>
-
-
-<!--$db['db_host'] = 'us-cdbr-iron-east-03.cleardb.net';
-$db['db_user'] = 'ba5909f71c5065';
-$db['db_pass'] = '964b71d0';
-$db['db_name'] = 'heroku_404e34a302806a2'; -->
