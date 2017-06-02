778da59151bd6acbdbc472fbba67d61a40556677
qcollinscode <qcollins@qraynix.com>
2017-06-02 13:31:16 -0700

    fixed message bug

--- src/includes/add_message.php
+++ src/includes/add_message.php
@@ -7,7 +7,7 @@
         $message_content = mysqli_real_escape_string($connection, $_POST["message_content"]);
         $message_date = date('d-m-y');
 
-        $query = "INSERT INTO messages(name, message_date, message_email, message_content) ";
+        $query = "INSERT INTO messages(message_name, message_date, message_email, message_content) ";
         $query .= "VALUES('{$message_name}', now(),'{$message_email}','{$message_content}')";
         $insert_message = mysqli_query($connection, $query);
 
