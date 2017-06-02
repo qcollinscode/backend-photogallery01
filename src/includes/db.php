046325042ba7275aa855a75f4159ff79140438c0
qcollinscode <qcollins@qraynix.com>
2017-06-02 12:49:20 -0700

    database error

--- src/includes/db.php
+++ src/includes/db.php
@@ -2,9 +2,9 @@
 
 
 $db['db_host'] = 'us-cdbr-iron-east-03.cleardb.net';
-$db['db_user'] = 'ba5909f71c5065';
-$db['db_pass'] = '964b71d0';
-$db['db_name'] = 'heroku_404e34a302806a2';
+$db['db_user'] = 'bcfe1056364b46';
+$db['db_pass'] = '322db624';
+$db['db_name'] = 'heroku_d0f8b8720ea3c95';
 
 
 
